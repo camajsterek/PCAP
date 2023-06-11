46ddc9db532e1e4b36f477404e2e2d6a02809090
camajsterek <camajsterek+github@gmail.com>
2023-06-11 10:58:17 +0100

    Updated Naming

---
+++ Casear Cypher.py
@@ -0,0 +1,5 @@
+# A program that takes a users input and shifts the english letters by the corresponding cipher input.
+# Numbers, case and spaces are preserved, non-english characters may display unusual properties.
+
+text = input("Please enter your message to be encrypted: ")
+shift = int(input("Please enter the encryption number: "))
